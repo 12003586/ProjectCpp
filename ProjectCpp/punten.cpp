3a7b1b39ffbc575d02af2fb91100da1575a2478b
Bo Mengels <12003586@student.pxl.be>
2021-12-05 22:05:26 +0100

    05/12

--- ProjectCpp/punten.cpp
+++ ProjectCpp/punten.cpp
@@ -10,5 +10,35 @@ punten::punten(QGraphicsItem* parent) : QGRaphicsTextItem(parent)
 void punten::verhoogScore()
 {
     //min 10:18 verdergaan
-    emit verhoogScore();
+    score += 50;
+    setPlainText(QString("Healt: ") + QString::number(health) + \n + QString("Score: ")+ QString::number(score));
+}
+
+void punten::verlaagScore()
+{
+    score -=50;
+    setPlainText(QString("Healt: ") + QString::number(health) + \n + QString("Score: ")+ QString::number(score));
+}
+
+int punten::getHealth() const
+{
+    return health;
+}
+
+void punten::verlaagHealth()
+{
+    health --;
+    setPlainText(QString("Healt: ") + QString::number(health) + \n + QString("Score: ")+ QString::number(score));
+}
+
+int punten::getScore()
+{
+    return score;
+}
+
+void punten::reset()
+{
+    score = 0;
+    health = maxHealth;
+    setPlainText(QString("Healt: ") + QString::number(health) + \n + QString("Score: ")+ QString::number(score));
 }
